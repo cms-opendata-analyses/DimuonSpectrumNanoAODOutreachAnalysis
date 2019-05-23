219a8b9d8f5f6cd2561af740283b8c4b1c7d2751
Stefan Wunsch <stefan.wunsch@cern.ch>
2019-05-23 13:36:12 +0200

    add files and adapt readme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # DimuonSpectrum2012NanoAOD
 Analysis using reduced NanoAOD files created from 2012 CMS open data producing a high statistics di-muon spectrum
+
+## How to run this?
+
+The analysis needs solely a ROOT installation (6.16 or greater). You can get the software easily using the CMS Open Data VM and CVMFS. Just run the following command in the terminal to source an appropriate software stack:
+
+```bash
+source /cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-slc6-gcc8-opt/setup.sh
+```
+
+The analysis code itself is provided in Python and C++. The instructions to run the scripts is shown below:
+
+**Python (interactive):**
+
+Note the `-i` flag, which keeps the Python interpreter alive after the end of the script so that the interactive plot can still be explored.
+
+```bash
+python -i dimuonSpectrum.py
+```
+
+**C++ (interactive):**
+
+```bash
+root -l dimuonSpectrum.C
+```
+
+**C++ (compiled):**
+
+Note that you have to select the compiler based on your system.
+
+```bash
+g++ -O3 -o dimuonSpectrum dimuonSpectrum.C $(root-config --cflags --libs)
+./dimuonSpectrum
+```
---
+++ dimuonSpectrum.py
@@ -0,0 +1,71 @@
+import ROOT
+
+# Enable multi-threading
+ROOT.ROOT.EnableImplicitMT()
+
+# Create dataframe from NanoAOD files
+df = ROOT.ROOT.RDataFrame("Events", "Run2012BC_DoubleMuParked.root")
+
+# Select events with exactly two muons
+df_2mu = df.Filter("nMuon == 2", "Events with exactly two muons")
+
+# Select events with two muons of opposite charge
+df_os = df_2mu.Filter("Muon_charge[0] != Muon_charge[1]", "Muons with opposite charge")
+
+# Compute invariant mass of the dimuon system
+ROOT.gInterpreter.Declare(
+"""
+using namespace ROOT::VecOps;
+float computeInvariantMass(RVec<float>& pt, RVec<float>& eta, RVec<float>& phi, RVec<float>& mass) {
+    ROOT::Math::PtEtaPhiMVector m1(pt[0], eta[0], phi[0], mass[0]);
+    ROOT::Math::PtEtaPhiMVector m2(pt[1], eta[1], phi[1], mass[1]);
+    return (m1 + m2).mass();
+}
+""")
+df_mass = df_os.Define("Dimuon_mass", "computeInvariantMass(Muon_pt, Muon_eta, Muon_phi, Muon_mass)")
+
+# Book histogram of dimuon mass spectrum
+bins = 30000 # Number of bins in the histogram
+low = 0.25 # Lower edge of the histogram
+up = 300.0 # Upper edge of the histogram
+hist = df_mass.Histo1D(ROOT.ROOT.RDF.TH1DModel("", "", bins, low, up), "Dimuon_mass")
+
+# Request cut-flow report
+report = df_mass.Report()
+
+# Create canvas for plotting
+ROOT.gStyle.SetOptStat(0)
+ROOT.gStyle.SetTextFont(42)
+c = ROOT.TCanvas("c", "", 800, 700)
+c.SetLogx()
+c.SetLogy()
+
+# Draw histogram
+hist.GetXaxis().SetTitle("m_{#mu#mu} (GeV)")
+hist.GetXaxis().SetTitleSize(0.04)
+hist.GetYaxis().SetTitle("N_{Events}")
+hist.GetYaxis().SetTitleSize(0.04)
+hist.DrawClone()
+
+# Draw labels
+label = ROOT.TLatex()
+label.SetTextAlign(22)
+label.DrawLatex(0.55, 3.0e4, "#eta")
+label.DrawLatex(0.77, 7.0e4, "#rho,#omega")
+label.DrawLatex(1.20, 4.0e4, "#phi")
+label.DrawLatex(4.40, 1.0e5, "J/#psi")
+label.DrawLatex(4.60, 1.0e4, "#psi'")
+label.DrawLatex(12.0, 2.0e4, "Y(1,2,3S)")
+label.DrawLatex(91.0, 1.5e4, "Z")
+label.SetNDC(True)
+label.SetTextAlign(11)
+label.SetTextSize(0.04)
+label.DrawLatex(0.10, 0.92, "#bf{CMS Open Data}")
+label.SetTextAlign(31)
+label.DrawLatex(0.90, 0.92, "#sqrt{s} = 8 TeV, L_{int} = 11.6 fb^{-1}")
+
+# Save plot
+c.SaveAs("dimuonSpectrum.pdf")
+
+# Print cut-flow report
+report.Print()
